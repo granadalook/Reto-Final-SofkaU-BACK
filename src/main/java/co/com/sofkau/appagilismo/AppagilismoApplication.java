4a5370394d4f72f426e19d56e30eb3f8dbf38b89
granadalook <tiven17.jsga@gmail.com>
2022-06-24 17:59:39 -0500

    FIRST COMMIT:  Se inicializa el proyecto con la estructura base , con las dependencia necesarias y los paquetes   limpios para su utilización

---
+++ src/main/java/co/com/sofkau/appagilismo/AppagilismoApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.appagilismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppagilismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppagilismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofkau/appagilismo/AppagilismoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.appagilismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppagilismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

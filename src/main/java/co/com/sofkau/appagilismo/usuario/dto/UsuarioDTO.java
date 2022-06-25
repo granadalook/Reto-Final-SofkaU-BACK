df93f9870e295532fefdf4906e308811b872a9d4
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-25 09:22:10 -0500

    CREATE: Se crea la clase UsuarioDTO con sus atributos, constructores y getter and setter.

--- src/main/java/co/com/sofkau/appagilismo/usuario/dto/UsuarioDTO.java
+++ src/main/java/co/com/sofkau/appagilismo/usuario/dto/UsuarioDTO.java
@@ -1,4 +1,67 @@
 package co.com.sofkau.appagilismo.usuario.dto;
 
+import org.springframework.data.mongodb.core.index.Indexed;
+
 public class UsuarioDTO {
+
+
+    private String usuarioId;
+
+
+    private String nombreCompleto;
+
+
+    private String email;
+
+
+    private String password;
+
+
+    private Set<String> proyectosAsociados;
+
+    public UsuarioDTO(String nombreCompleto, String email, String password) {
+        this.nombreCompleto = nombreCompleto;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getProyectosAsociados() {
+        return proyectosAsociados;
+    }
+
+    public void setProyectosAsociados(Set<String> proyectosAsociados) {
+        this.proyectosAsociados = proyectosAsociados;
+    }
 }

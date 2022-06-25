b08fd8d534b341fcf2eec2f4140612fc41f8ed12
gandalfblue <casanova01_ahotmail.com>
2022-06-25 16:59:58 -0500

    CREATE-- Se crean los test para el caso de uso de CrearUsuarioCasoDeUso y para la ruta de CrearUsuario.

---
+++ src/test/java/co/com/sofkau/appagilismo/usuario/casos_de_uso/CrearUsuarioCasoDeUsoTest.java
@@ -0,0 +1,78 @@
+package co.com.sofkau.appagilismo.usuario.casos_de_uso;
+
+import co.com.sofkau.appagilismo.usuario.coleccion.Usuario;
+import co.com.sofkau.appagilismo.usuario.dto.UsuarioDTO;
+import co.com.sofkau.appagilismo.usuario.mapper.MapperUsuario;
+import co.com.sofkau.appagilismo.usuario.repositorio.UsuarioRepositorio;
+import co.com.sofkau.appagilismo.usuario.utilidades.EnviarMail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CrearUsuarioCasoDeUsoTest {
+
+    @Autowired
+    private UsuarioRepositorio repositorio;
+    @MockBean
+    private EnviarMail enviarMail;
+    @Autowired
+    private CrearUsuarioCasoDeUso casoDeUso;
+    @Autowired
+    private MapperUsuario mapperUsuario;
+    private static final String ID = "U-0001";
+    private static final String NOMBRECOMPLETO = "Andres Felipe Lozada";
+    private static final String EMAIL = "ciendecilantro.agile@gmail.com";
+    private static final String PASSWORD = "123456789";
+    private static final String ROL = "Desarrollador";
+
+    @BeforeEach
+    public void configuracionInicial() {
+        repositorio = mock(UsuarioRepositorio.class);
+        casoDeUso = new CrearUsuarioCasoDeUso(repositorio, mapperUsuario);
+        enviarMail = mock(EnviarMail.class);
+    }
+
+    @Test
+    void crearUsuarioTest() {
+
+        var usuario = new Usuario();
+        usuario.setUsuarioId(ID);
+        usuario.setNombreCompleto(NOMBRECOMPLETO);
+        usuario.setEmail(EMAIL);
+        usuario.setPassword(PASSWORD);
+        usuario.setRol(ROL);
+
+        var usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setUsuarioId(ID);
+        usuarioDTO.setNombreCompleto(NOMBRECOMPLETO);
+        usuarioDTO.setEmail(EMAIL);
+        usuarioDTO.setPassword(PASSWORD);
+        usuarioDTO.setRol(ROL);
+
+        when(repositorio.save(Mockito.any(Usuario.class))).thenReturn(Mono.just(usuario));
+
+        StepVerifier.create(casoDeUso.crearUsuario(usuarioDTO))
+                .expectNextMatches(user -> {
+                    assert user.getUsuarioId().equalsIgnoreCase(usuario.getUsuarioId());
+                    assert user.getNombreCompleto().equalsIgnoreCase(usuario.getNombreCompleto());
+                    assert user.getEmail().equalsIgnoreCase(usuario.getEmail());
+                    assert user.getPassword().equalsIgnoreCase(usuario.getPassword());
+                    assert user.getRol().equalsIgnoreCase(usuario.getRol());
+                    return true;
+                })
+                .verifyComplete();
+
+        verify(repositorio).save(Mockito.any(Usuario.class));
+    }
+}
---
+++ src/test/java/co/com/sofkau/appagilismo/usuario/rutas/UsuarioRutasTest.java
@@ -0,0 +1,81 @@
+package co.com.sofkau.appagilismo.usuario.rutas;
+
+import co.com.sofkau.appagilismo.usuario.casos_de_uso.CrearUsuarioCasoDeUso;
+import co.com.sofkau.appagilismo.usuario.coleccion.Usuario;
+import co.com.sofkau.appagilismo.usuario.dto.UsuarioDTO;
+import co.com.sofkau.appagilismo.usuario.mapper.MapperUsuario;
+import co.com.sofkau.appagilismo.usuario.repositorio.UsuarioRepositorio;
+import co.com.sofkau.appagilismo.usuario.utilidades.EnviarMail;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.Mockito.verify;
+import static reactor.core.publisher.Mono.when;
+
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {UsuarioRutas.class, CrearUsuarioCasoDeUso.class, MapperUsuario.class})
+class UsuarioRutasTest {
+
+    @MockBean
+    private UsuarioRepositorio repositorio;
+
+    @MockBean
+    private EnviarMail enviarMail;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private static final String ID = "U-0001";
+    private static final String NOMBRECOMPLETO = "Andres Felipe Lozada";
+    private static final String EMAIL = "ciendecilantro.agile@gmail.com";
+    private static final String PASSWORD = "123456789";
+    private static final String ROL = "Desarrollador";
+
+    @Test
+    void usuarioRutasTest() {
+
+        var usuario = new Usuario();
+        usuario.setUsuarioId(ID);
+        usuario.setNombreCompleto(NOMBRECOMPLETO);
+        usuario.setEmail(EMAIL);
+        usuario.setPassword(PASSWORD);
+        usuario.setRol(ROL);
+
+        var usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setUsuarioId(ID);
+        usuarioDTO.setNombreCompleto(NOMBRECOMPLETO);
+        usuarioDTO.setEmail(EMAIL);
+        usuarioDTO.setPassword(PASSWORD);
+        usuarioDTO.setRol(ROL);
+
+        Mockito.when(repositorio.save(Mockito.any(Usuario.class))).thenReturn(Mono.just(usuario));
+
+        webTestClient.post()
+                .uri("/usuario/crear")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(usuarioDTO), UsuarioDTO.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UsuarioDTO.class)
+                .value(respuesta -> {
+                    Assertions.assertEquals(respuesta.getUsuarioId(), usuario.getUsuarioId());
+                    Assertions.assertEquals(respuesta.getNombreCompleto(), usuario.getNombreCompleto());
+                    Assertions.assertEquals(respuesta.getEmail(), usuario.getEmail());
+                    Assertions.assertEquals(respuesta.getPassword(), usuario.getPassword());
+                    Assertions.assertEquals(respuesta.getRol(), usuario.getRol());
+                });
+        verify(repositorio).save(Mockito.any(Usuario.class));
+    }
+}

4c936b17bf9676cd21f183157bc441cbeaa3519c
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-26 17:53:54 -0500

    CREATE: Se inserta la configuracion para que permita el acceso al localhost del front.

--- src/main/java/co/com/sofkau/appagilismo/usuario/casos_de_uso/CrearUsuarioCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/usuario/casos_de_uso/CrearUsuarioCasoDeUso.java
@@ -7,13 +7,11 @@ import co.com.sofkau.appagilismo.usuario.utilidades.EnviarMail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
-import java.util.Objects;
+
 
 @Service
 @Validated
--- src/main/java/co/com/sofkau/appagilismo/usuario/rutas/UsuarioRutas.java
+++ src/main/java/co/com/sofkau/appagilismo/usuario/rutas/UsuarioRutas.java
@@ -8,19 +8,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-
 import java.util.function.Function;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
+@CrossOrigin("http://localhost:4200")
 public class UsuarioRutas {
 
     @Bean

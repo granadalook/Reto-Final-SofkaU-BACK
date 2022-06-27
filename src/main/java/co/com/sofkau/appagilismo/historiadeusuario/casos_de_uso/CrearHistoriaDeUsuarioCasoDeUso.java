175597fe4dac330f46f3b3b9907b8134468490f8
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-27 16:05:10 -0500

    CREATE: Se crea el package repositorio junto con la interface correspondiente a HistoriaDeUsuarioRepositorio.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/CrearHistoriaDeUsuarioCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/CrearHistoriaDeUsuarioCasoDeUso.java
@@ -1,4 +1,10 @@
 package co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso;
 
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
 public class CrearHistoriaDeUsuarioCasoDeUso {
+
 }

6bc65db6321ce08403543251bb77b9ade194bc72
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-27 16:25:23 -0500

    CREATE: Se crea caso de uso para eliminar Historia de Usuario, pero se deja comentada debido a que es necesario que ya exista la clase Tarea para su correcto funcionamiento.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/EliminarHistoriaDeUsuarioCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/EliminarHistoriaDeUsuarioCasoDeUso.java
@@ -1,2 +1,30 @@
-package co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso;public class EliminarHistoriaDeUsuarioCasoDeUso {
-}
+package co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso;
+
+import co.com.sofkau.appagilismo.historiadeusuario.repositorio.HistoriaDeUsuarioRepositorio;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+/*@Service
+@Validated
+public class EliminarHistoriaDeUsuarioCasoDeUso implements Function<String, Mono<Void>> {
+
+    private final HistoriaDeUsuarioRepositorio repositorio;
+
+    //private final TareaRepositorio tareaRepositorio;
+
+    public EliminarHistoriaDeUsuarioCasoDeUso(HistoriaDeUsuarioRepositorio repositorio){ // añadir dentro TareaRepositorio tareaRepositorio
+        this.repositorio = repositorio;
+        //this.tareaRepositorio=tareaRepositorio;
+    }
+
+    *//*@Override
+    public Mono<Void> apply(String id) {
+        Objects.requireNonNull(id, "Id es campo obligatorio.");
+        return repositorio.deleteById(id)
+                .switchIfEmpty(Mono.defer(() -> tareaRepositorio.eliminarPorHistoriaUsuarioId(id)));
+    }*//*
+}*/

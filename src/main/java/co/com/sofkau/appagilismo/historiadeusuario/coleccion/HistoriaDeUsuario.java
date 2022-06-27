8c99d0c25135b89b8fc70e4bb7743a5d8cc2abd6
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-27 14:33:59 -0500

    CREATE: Se crea la clase Historia de usuario con sus atributos, constructores y getter and setter.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/coleccion/HistoriaDeUsuario.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/coleccion/HistoriaDeUsuario.java
@@ -1,2 +1,56 @@
-package co.com.sofkau.appagilismo.historiadeusuario.coleccion;public class HistoriaDeUsuario {
+package co.com.sofkau.appagilismo.historiadeusuario.coleccion;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Document(collection = "historiadeusuario")
+public class HistoriaDeUsuario {
+    @Id
+    private String historiaUsuarioId;
+
+    @NotBlank
+    private String Descripcion;
+
+    @NotBlank
+    private String usuarioId;
+
+    public HistoriaDeUsuario() {
+    }
+
+    public HistoriaDeUsuario(String historiaUsuarioId, String descripcion, String usuarioId) {
+        this.historiaUsuarioId = historiaUsuarioId;
+        Descripcion = descripcion;
+        this.usuarioId = usuarioId;
+    }
+
+    public HistoriaDeUsuario(String descripcion, String usuarioId) {
+        Descripcion = descripcion;
+        this.usuarioId = usuarioId;
+    }
+
+    public String getHistoriaUsuarioId() {
+        return historiaUsuarioId;
+    }
+
+    public void setHistoriaUsuarioId(String historiaUsuarioId) {
+        this.historiaUsuarioId = historiaUsuarioId;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public String getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
 }

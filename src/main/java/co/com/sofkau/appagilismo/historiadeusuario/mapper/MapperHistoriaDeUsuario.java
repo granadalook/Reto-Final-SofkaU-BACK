bf821d433e5f59092c7e7e8bce5b357dc2a32bd2
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-27 15:42:45 -0500

    CREATE: Se crea la clase MapperHistoriaDeUsuario para poder convertir el objeto HistoriaDeUsuario a HistoriaDeUusarioDTO y viceversa.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/mapper/MapperHistoriaDeUsuario.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/mapper/MapperHistoriaDeUsuario.java
@@ -1,2 +1,29 @@
-package co.com.sofkau.appagilismo.historiadeusuario.mapper;public class MapperHistoriaDeUsuario {
+package co.com.sofkau.appagilismo.historiadeusuario.mapper;
+
+import co.com.sofkau.appagilismo.historiadeusuario.coleccion.HistoriaDeUsuario;
+import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class MapperHistoriaDeUsuario {
+
+    public Function<HistoriaDeUsuarioDTO, HistoriaDeUsuario> mapperAHistoriaDeUsuario(String id){
+        return updateHistoriaDeUsuario -> {
+            var historiaDeUsuario = new HistoriaDeUsuario();
+            historiaDeUsuario.setHistoriaUsuarioId(id);
+            historiaDeUsuario.setDescripcion(updateHistoriaDeUsuario.getDescripcion());
+            historiaDeUsuario.setUsuarioId(updateHistoriaDeUsuario.getUsuarioId());
+            return historiaDeUsuario;
+        };
+    }
+
+    public Function<HistoriaDeUsuario, HistoriaDeUsuarioDTO> mapperAHistoriaDeUsuarioDTO(){
+        return entity -> new HistoriaDeUsuarioDTO(
+                entity.getHistoriaUsuarioId(),
+                entity.getDescripcion(),
+                entity.getUsuarioId()
+        );
+    }
 }

988aa6a258cf09ec36f004725cff71e249c5b6a3
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-28 10:12:07 -0500

    CREATE: Se agrega al HistoriaDeUsuarioRepositorio el metodo que permite traer todas las historias de usuario asociadas a un UsuarioId.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/repositorio/HistoriaDeUsuarioRepositorio.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/repositorio/HistoriaDeUsuarioRepositorio.java
@@ -3,7 +3,10 @@ package co.com.sofkau.appagilismo.historiadeusuario.repositorio;
 import co.com.sofkau.appagilismo.historiadeusuario.coleccion.HistoriaDeUsuario;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface HistoriaDeUsuarioRepositorio  extends ReactiveMongoRepository<HistoriaDeUsuario, String>{
+
+    Flux<HistoriaDeUsuario> buscarPorUsuarioId(String usuarioId);
 }

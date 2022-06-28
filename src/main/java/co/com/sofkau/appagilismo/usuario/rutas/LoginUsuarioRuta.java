183ec076ce66b624f57fc8acc618e7d22be11dcf
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-28 12:18:57 -0500

    CREATE: se redistribuye las rutas existentes de usuario en una clase individual // se crea la ruta para llamar al metodo que lista todas las tareas asociadas a una historia de usuario.

--- src/main/java/co/com/sofkau/appagilismo/usuario/rutas/LoginUsuarioRuta.java
+++ src/main/java/co/com/sofkau/appagilismo/usuario/rutas/LoginUsuarioRuta.java
@@ -1,40 +1,23 @@
 package co.com.sofkau.appagilismo.usuario.rutas;
 
-
-import co.com.sofkau.appagilismo.usuario.casos_de_uso.CrearUsuarioCasoDeUso;
 import co.com.sofkau.appagilismo.usuario.casos_de_uso.LogInCasoDeUso;
-import co.com.sofkau.appagilismo.usuario.dto.UsuarioDTO;
 import co.com.sofkau.appagilismo.usuario.dto.UsuarioLogin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class UsuarioRutas {
+public class LoginUsuarioRuta {
 
-    @Bean
-    public RouterFunction<ServerResponse> crearUsuario(CrearUsuarioCasoDeUso crearUsuarioCasoDeUso){
-        Function<UsuarioDTO, Mono<ServerResponse>> crearUsuario = usuarioDTO -> crearUsuarioCasoDeUso.crearUsuario(usuarioDTO)
-                .flatMap(resultado -> ServerResponse.ok()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .bodyValue(resultado)
-                );
-        return route(POST("/usuario/crear").and(accept(MediaType.APPLICATION_JSON)),
-            request -> request.bodyToMono(UsuarioDTO.class)
-                    .flatMap(crearUsuario)
-        );
-    }
     @Bean
     public RouterFunction<ServerResponse> Login(LogInCasoDeUso logInCasoDeUso){
         Function<UsuarioLogin, Mono<ServerResponse>> logIn = usuarioLogin -> logInCasoDeUso.logIn(usuarioLogin)
---
+++ src/main/java/co/com/sofkau/appagilismo/usuario/rutas/ObtenerTareasPorHistoriaUsuarioIdRuta.java
@@ -0,0 +1,30 @@
+package co.com.sofkau.appagilismo.usuario.rutas;
+
+import co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso.AsociarHistoriaDeUsuarioCasoDeUso;
+import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Configuration
+public class ObtenerTareasPorHistoriaUsuarioIdRuta {
+
+    @Bean
+    public RouterFunction<ServerResponse> obtenerTareasPorHistoriaUsuarioId(AsociarHistoriaDeUsuarioCasoDeUso asociarHistoriaDeUsuarioCasoDeUso){
+        return route(GET("/historia/obtenerTareas/{id}").and(accept(APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(asociarHistoriaDeUsuarioCasoDeUso.apply(
+                                request.pathVariable("id")),
+                                HistoriaDeUsuarioDTO.class
+                                ))
+                );
+    }
+}

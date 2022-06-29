bac46611784d545478bd24599dabbcfaa2409ca8
gandalfblue <casanova01_ahotmail.com>
2022-06-29 12:58:28 -0500

    CREATE-- Se implementa el caso de uso de eliminar proyecto por id.

--- src/main/java/co/com/sofkau/appagilismo/proyecto/casos_de_uso/EliminarProyectoCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/proyecto/casos_de_uso/EliminarProyectoCasoDeUso.java
@@ -3,10 +3,14 @@ package co.com.sofkau.appagilismo.proyecto.casos_de_uso;
 import co.com.sofkau.appagilismo.historiadeusuario.repositorio.HistoriaDeUsuarioRepositorio;
 import co.com.sofkau.appagilismo.proyecto.mapper.MapperProyecto;
 import co.com.sofkau.appagilismo.proyecto.repositorio.ProyectoRepositorio;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
 import java.util.Objects;
 
+@Service
+@Validated
 public class EliminarProyectoCasoDeUso implements EliminarCasoDeUsoInterface {
 
     private final ProyectoRepositorio repositorio;
@@ -20,9 +24,9 @@ public class EliminarProyectoCasoDeUso implements EliminarCasoDeUsoInterface {
     }
 
     @Override
-    public Mono<Void> eliminarProyecto(String idProyecto) {
+    public Mono<Void> deleteProyectoById(String idProyecto) {
         Objects.requireNonNull(idProyecto, "Id es requerido");
         return repositorio.deleteById(idProyecto)
-                .switchIfEmpty(Mono.defer(() -> historiaDeUsuarioRepositorio.eliminarByProyectoId(idProyecto)));
+                .switchIfEmpty(Mono.defer(() -> historiaDeUsuarioRepositorio.deleteByProyectoId(idProyecto)));
     }
 }

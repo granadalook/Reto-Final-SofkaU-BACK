d1fe1fb09e7448811f7ddd9373ad9a2e4de30596
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-29 12:22:29 -0500

    CREATE: Se crea caso de uso para actualizar Historia de Usuario.

---
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/ActualizarHistoriaDeUsuarioCasoDeUso.java
@@ -0,0 +1,35 @@
+package co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso;
+
+import co.com.sofkau.appagilismo.historiadeusuario.coleccion.HistoriaDeUsuario;
+import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
+import co.com.sofkau.appagilismo.historiadeusuario.mapper.MapperHistoriaDeUsuario;
+import co.com.sofkau.appagilismo.historiadeusuario.repositorio.HistoriaDeUsuarioRepositorio;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+@Validated
+public class ActualizarHistoriaDeUsuarioCasoDeUso implements Function<String, Mono<HistoriaDeUsuarioDTO>> {
+
+    private final HistoriaDeUsuarioRepositorio repositorio;
+
+    private final MapperHistoriaDeUsuario mapperHistoriaDeUsuario;
+
+    public ActualizarEstadoHistoriaDeUsuarioCasoDeUso(HistoriaDeUsuarioRepositorio repositorio, MapperHistoriaDeUsuario mapperHistoriaDeUsuario){
+        this.repositorio=repositorio;
+        this.mapperHistoriaDeUsuario=mapperHistoriaDeUsuario;
+    }
+
+
+    @Override
+    public Mono<HistoriaDeUsuarioDTO> apply(HistoriaDeUsuarioDTO historiaDeUsuarioDTO) {
+        Objects.requireNonNull(historiaDeUsuarioDTO.getHistoriaUsuarioId(), "Id de la Historia De Usuario es requerido.");
+        return repositorio.save(mapperHistoriaDeUsuario.mapperAHistoriaDeUsuario(historiaDeUsuarioDTO.getHistoriaUsuarioId()).apply(historiaDeUsuarioDTO))
+                .map(HistoriaDeUsuario::getHistoriaUsuarioId);
+        
+    }
+}

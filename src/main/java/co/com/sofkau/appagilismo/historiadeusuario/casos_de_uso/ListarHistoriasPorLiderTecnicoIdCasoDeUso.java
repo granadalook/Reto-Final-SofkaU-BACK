d159c8556779002d2f9d3b210ba77e7f0a15b09e
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-30 14:24:40 -0500

    FIX: Se manejan los errores en el caso de que no llegue un id o me llegue un id que no existe ademas que se organiza para que retorne las historias de usuario junto con las tareas asociadas al interior del  caso de uso de listar por id desarrollador.

--- src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/ListarHistoriasPorLiderTecnicoIdCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/historiadeusuario/casos_de_uso/ListarHistoriasPorLiderTecnicoIdCasoDeUso.java
@@ -1,11 +1,16 @@
 package co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso;
 
+import co.com.sofkau.appagilismo.excepciones.ExcepcionPersonalizadaInternalServerError;
+import co.com.sofkau.appagilismo.excepciones.ExcepcionPersonalizadaNotFound;
 import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
 import co.com.sofkau.appagilismo.historiadeusuario.mapper.MapperHistoriaDeUsuario;
 import co.com.sofkau.appagilismo.historiadeusuario.repositorio.HistoriaDeUsuarioRepositorio;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
@@ -27,6 +32,14 @@ public class ListarHistoriasPorLiderTecnicoIdCasoDeUso implements Function<Strin
     @Override
     public Flux<HistoriaDeUsuarioDTO> apply(String liderTecnicoId) {
         return repositorio.findAllByLiderTecnicoId(liderTecnicoId)
-                .map(mapperHistoriaDeUsuario.mapperAHistoriaDeUsuarioDTO());
+                .map(mapperHistoriaDeUsuario.mapperAHistoriaDeUsuarioDTO())
+                .flatMap(mapperHistoriaDeUsuario.mapperHistoriaDeUsuarioActualizada())
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .onErrorResume(error -> {
+                    if (error.getMessage().equals("404 NOT_FOUND")) {
+                        return Mono.error(new ExcepcionPersonalizadaNotFound("Usuario no se encuentra registrado"));
+                    }
+                    return Mono.error(new ExcepcionPersonalizadaInternalServerError("Campos vacios"));
+                });
     }
 }

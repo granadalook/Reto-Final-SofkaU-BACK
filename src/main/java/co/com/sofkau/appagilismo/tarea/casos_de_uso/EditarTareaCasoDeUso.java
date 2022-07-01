304a6b49560017dea4563332c80e213aaacd154f
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-30 19:41:38 -0500

    FIX: Se corrige error en el guardado al momento de actualizar una tarea ya existente.

--- src/main/java/co/com/sofkau/appagilismo/tarea/casos_de_uso/EditarTareaCasoDeUso.java
+++ src/main/java/co/com/sofkau/appagilismo/tarea/casos_de_uso/EditarTareaCasoDeUso.java
@@ -1,16 +1,14 @@
 package co.com.sofkau.appagilismo.tarea.casos_de_uso;
 
 import co.com.sofkau.appagilismo.historiadeusuario.casos_de_uso.AsociarHistoriaDeUsuarioCasoDeUso;
-import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
 import co.com.sofkau.appagilismo.historiadeusuario.mapper.MapperHistoriaDeUsuario;
-import co.com.sofkau.appagilismo.tarea.colleccion.Tarea;
 import co.com.sofkau.appagilismo.tarea.dto.TareaDTO;
 import co.com.sofkau.appagilismo.tarea.mapper.MapperTarea;
 import co.com.sofkau.appagilismo.tarea.repositorio.TareaRepositorio;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 @Service
 @Validated
 public class EditarTareaCasoDeUso implements EditarTareaInterface {
@@ -28,29 +26,11 @@ public class EditarTareaCasoDeUso implements EditarTareaInterface {
 
 
     @Override
-    public Mono<HistoriaDeUsuarioDTO> editarTarea(TareaDTO tareaDTO) {
-        return asociarHistoriaDeUsuarioCasoDeUso.apply(tareaDTO.getHistoriaUsuarioId())
-                .flatMap(historiadeUsuario ->
-                        tareaRepositorio.save(mapperTarea.mapperATarea(tareaDTO.getTareaId()).apply(tareaDTO))
-                                .map(tarea -> {
-                                    historiadeUsuario.getTareas().add(
-                                            mapperTarea.mapperATareaDTO().apply(tarea)
-                                    );
-                                    return historiadeUsuario;
-                                })
-                );
-
+    public Mono<TareaDTO> editarTarea(TareaDTO tareaDTO) {
+        return asociarHistoriaDeUsuarioCasoDeUso.apply(tareaDTO.getHistoriaUsuarioId()).flatMap(historiaDeUsuario ->
+                tareaRepositorio.save(mapperTarea.mapperATarea(tareaDTO.getTareaId()).apply(tareaDTO))
+                        .map(mapperTarea.mapperATareaDTO())
+        );
     }
 }
-       /*
-return getUseCase.apply(answerDTO.getQuestionId())
-                .flatMap(question ->
-                        answerRepository.save(mapperUtilsAnswer.mapperToAnswer().apply(answerDTO))
-                                .map(answer -> {
-                                    question.getAnswers().add(
-                                            mapperUtilsAnswer.mapEntityToAnswer().apply(answer)
-                                    );
-                                    return question;
-                                })
-                );
-         */
+
--- src/main/java/co/com/sofkau/appagilismo/tarea/casos_de_uso/EditarTareaInterface.java
+++ src/main/java/co/com/sofkau/appagilismo/tarea/casos_de_uso/EditarTareaInterface.java
@@ -1,15 +1,12 @@
 package co.com.sofkau.appagilismo.tarea.casos_de_uso;
 
-import co.com.sofkau.appagilismo.historiadeusuario.dto.HistoriaDeUsuarioDTO;
-import co.com.sofkau.appagilismo.tarea.colleccion.Tarea;
 import co.com.sofkau.appagilismo.tarea.dto.TareaDTO;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 
 @FunctionalInterface
 public interface EditarTareaInterface {
-    Mono<HistoriaDeUsuarioDTO> editarTarea (@Valid TareaDTO tareaDTO);
+    Mono<TareaDTO> editarTarea (@Valid TareaDTO tareaDTO);
 
 }
